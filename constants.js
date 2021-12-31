f0d667688c70fe9924716078e6698dd5cc449007
Taio Newgate <vinhyenvodoi98@gmail.com>
2021-12-31 20:14:26 +0700

    fix env

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const SLACK_OAUTH_TOKEN = process.env.OAUTH_TOKEN
+const SLACK_OAUTH_TOKEN = process.env.SLACK_OAUTH_TOKEN
 const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
 const BOT_SPAM_CHANNEL = '#test-bot'
 
